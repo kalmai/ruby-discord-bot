a48e3d9b55fc73d63b0490d6127b01dfd89354aa
kalmai <leozverres@gmail.com>
2021-08-07 00:59:56 -0400

    maybe all fixed now

--- hello.rb
+++ hello.rb
@@ -2,13 +2,13 @@ require 'dotenv'
 require 'discordrb'
 require 'pry'
 
-Dotenv.load('.config.env')
+Dotenv.load('.env')
 #change
 
 bot = Discordrb::Bot.new token: ENV['TOKEN']
 
 bot.message() do |event|
-  event.respond 'Pong!'
+  event.respond "#{event.message} Pong!"
 end
 
 bot.run
